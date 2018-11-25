b3f261d90f942600874d81a46e5083d4f562ddb6
m7hunter <mahmoudhawas0@gmail.com>
2018-11-25 11:50:50 +0200

    APIs updated

--- app/src/main/java/it_geeks/info/gawla_app/RESTful/APIs.java
+++ app/src/main/java/it_geeks/info/gawla_app/RESTful/APIs.java
@@ -7,17 +7,18 @@ import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.PUT;
 import retrofit2.http.Path;
 
 public interface APIs {
 
-    //user
-    @PUT("/User.json")
-    Call<ResponseBody> newUser(@Path("phone") String phone,
-                               @Body User user);
+    // register user
+    @POST("/register.json")
+    Call<ResponseBody> newUser(@Body User user);
 
-    @GET("/User/{phone}.json")
+    // login user
+    @GET("/login.json")
     Call<JsonElement> getUser(@Path("phone") String phone);
 
 }
