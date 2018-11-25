7faaaa06bfbc4e1553cb06820accf90e84025101
Mohamed Abdelhady <40372010+MohamedAbdelhadyPro@users.noreply.github.com>
2018-11-25 17:51:21 +0200

    login

--- app/src/main/java/it_geeks/info/gawla_app/RESTful/APIs.java
+++ app/src/main/java/it_geeks/info/gawla_app/RESTful/APIs.java
@@ -1,13 +1,16 @@
 package it_geeks.info.gawla_app.RESTful;
 
-import com.google.gson.JsonElement;
+
+import com.google.gson.JsonObject;
 
 import it_geeks.info.gawla_app.Models.User;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
-import retrofit2.http.GET;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.POST;
+
 
 public interface APIs {
 
@@ -16,7 +19,8 @@ public interface APIs {
     Call<ResponseBody> registerUser(@Body User user);
 
     // login user
-    @GET("/login.json")
-    Call<JsonElement> loginUser(String  email, String pass);
+    @FormUrlEncoded
+    @POST("login")
+    Call<JsonObject> loginUser(@Field("email") String email , @Field("password") String password);
 
 }
