b395e72a06268ffb2534493a929848d0bbac7e29
Mohamed Abdelhady <40372010+MohamedAbdelhadyPro@users.noreply.github.com>
2018-12-11 14:27:13 +0200

    social login

--- app/src/main/java/it_geeks/info/gawla_app/Repositry/Models/Request.java
+++ app/src/main/java/it_geeks/info/gawla_app/Repositry/Models/Request.java
@@ -20,7 +20,7 @@ public class Request {
 
     private String provider;
 
-    private int provider_id ;
+    private String provider_id ;
 
     private String provider_name ;
 
@@ -39,7 +39,7 @@ public class Request {
         this.password = pass;
     }
 
-    public Request(String provider, int provider_id, String provider_name, String provider_email, String provider_image) { // Login and Register SocialMedia Requesr
+    public Request(String provider, String provider_id, String provider_name, String provider_email, String provider_image) { // Login and Register SocialMedia Requesr
         this.provider = provider;
         this.provider_id = provider_id;
         this.provider_name = provider_name;
