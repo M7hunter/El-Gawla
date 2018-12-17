5d3092e3b8d0bcbdb0a9a436302f9cbfe0733df7
unknown <mahmoud.hawas@itgeeks.info>
2018-12-17 11:46:32 +0200

    add new pages 2

---
+++ app/src/main/java/it_geeks/info/gawla_app/Views/MenuOptions/NotificationActivity.java
@@ -0,0 +1,28 @@
+package it_geeks.info.gawla_app.Views.MenuOptions;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;;
+
+import it_geeks.info.gawla_app.R;
+
+public class NotificationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification);
+
+        initViews();
+    }
+
+    private void initViews() {
+        // back
+        findViewById(R.id.app_settings_back).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+    }
+}
