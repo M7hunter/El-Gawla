41eb3f4de54301628df8c1c190a17e255343db0b
Mohamed Abdelhady <40372010+MohamedAbdelhadyPro@users.noreply.github.com>
2019-02-20 18:04:59 +0200

    chat done

--- app/src/main/java/it_geeks/info/gawla_app/Repositry/SocketConnection/Constants.java
+++ app/src/main/java/it_geeks/info/gawla_app/Repositry/SocketConnection/Constants.java
@@ -7,6 +7,6 @@ public class Constants {
     // http://dev.itgeeks.info:8888
     // http://134.209.0.250:8888 (new)
 
-    public static final String GAWLA_SERVER_URL = "http://192.168.1.8:8888";
+    public static final String GAWLA_SERVER_URL = "http://134.209.0.250:8888";
 
 }
